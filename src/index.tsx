64c042adb384ddc330b7013f0434e1039de53a43
cristianocsl <cslcristiano@gmail.com>
2022-06-20 21:22:13 -0300

    feat: adiciona ChakraProvider ao index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Provider from './context/Provider';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <Provider>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
