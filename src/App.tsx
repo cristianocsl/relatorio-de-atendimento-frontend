1e2f7aa46467f058e2c6b25bba7837c13fe26f90
cristianocsl <cslcristiano@gmail.com>
2022-06-21 18:12:08 -0300

    feat: adiciona rotas de registro e de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import Register from './pages/register';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import BasePage from './pages/BaseLoginRegister';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/register" element={<Register />} />
+        <Route path="/" element={ <Navigate replace to="/login" /> } />
+          <Route path="/register" element={<BasePage title="Cadastro" hidden={false}/>} />
+          <Route path="/login" element={<BasePage title="Login" hidden={true}/>} />
         </Routes>
       </BrowserRouter>
     </div>
