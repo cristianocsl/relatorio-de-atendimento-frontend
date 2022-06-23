749d04172dceb1106d0f26ec1b9a937a9aa45240
cristianocsl <cslcristiano@gmail.com>
2022-06-23 18:19:36 -0300

    feat: adiciona tipagem de dados de entrada da função de registro de pacientes

--- src/services/types.ts
+++ src/services/types.ts
@@ -24,3 +24,27 @@ export type thisRespRegister = {
   userId: string,
   message: string,
 }
+
+export type thisPatient = {
+  patient: string,
+  neighborhood: string,
+  status: string,
+  priority: string,
+  days: number[],
+  serviceGoal: {
+    weekly: number,
+    monthly: number,
+  },
+  servicePerformed: {
+    weekly: number,
+    monthly: number,
+  },
+  servicePending: {
+    weekly: number,
+    monthly: number,
+  },
+  healthInsurance: string,
+  unitPrice: number,
+  totalPrice: number,
+  evolution: string,
+};
