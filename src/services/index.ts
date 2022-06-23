79e5d373501b5da252266281119bb89eb489fe88
cristianocsl <cslcristiano@gmail.com>
2022-06-22 23:52:51 -0300

    feat: adiciona requisição de login / register

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { thisLogin, thisName } from './types';
 
 const axiosInstance = axios.create({
   baseURL: 'https://minhaagendahomecare.herokuapp.com',
@@ -7,22 +8,37 @@ const axiosInstance = axios.create({
   },
 });
 
-interface login {
-  email: string,
-  password: string,
-}
-
-const login = async (userData: login) => {
+const wakeUp = async () => {
   try {
-    const { data } = await axiosInstance.post('/register', userData);
+    const { data } = await axiosInstance.get('/wakeup');
     return data;
   } catch (err: unknown) {
     return err;
+  }
+}
+
+const register = async (userData: thisLogin | thisName) => {
+  try {
+    const { data } = await axiosInstance.post('/register', userData);
+    return data;
+  } catch (err: any) {
+    return err.response.data.message;
+  }
+};
+
+const login = async (userData: thisLogin) => {
+  try {
+    const { data } = await axiosInstance.post('/login', userData);
+    return data;
+  } catch (err: any) {
+    return err.response.data.message;
   }
 };
 
 const axiosServices = {
   axiosInstance,
+  wakeUp,
+  register,
   login,
 };
 
