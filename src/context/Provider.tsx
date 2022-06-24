c5b403985086eded448e38926b6da8ecd73cac7b
cristianocsl <cslcristiano@gmail.com>
2022-06-24 02:02:11 -0300

    feat: adiciona ao provider array com todos os pacientes cadastrados

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,13 +1,26 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import axiosServices from "../services";
 import MyContext from "./MyContext";
+
 
 type Props = { children: ReactElement | ReactElement[] };
 
 const Provider = ({ children }: Props) => {
+  const [patients, setPatients] = useState([]);
+  useEffect(() => {
+    const getPatients = async () => {
+      const patients = await axiosServices.get();
+      setPatients(patients);
+      console.log(patients);
+    };
+
+    getPatients();
+  }, []);
   const hello = 'Hello Word!';
 
   const context = {
     hello,
+    patients,
   };
 
   return (
