a164d20c46ebaabb119826ce19d914e428472171
cristianocsl <cslcristiano@gmail.com>
2022-06-25 21:44:53 -0300

    fix: adiciona estado p verificar que a requisição get só poderá ser feita se login for confirmado

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -8,20 +8,24 @@ type Props = { children: ReactElement | ReactElement[] };
 
 const Provider = ({ children }: Props) => {
   const [patients, setPatients] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     const getPatients = async () => {
-      const patients = await axiosServices.get();
-      setPatients(patients);
+      if (isLoggedIn) {
+        const patients = await axiosServices.get();
+        setPatients(patients);
+      }
     };
 
     getPatients();
-  }, []);
+  }, [isLoggedIn]);
 
   const patientsByDay = (day: number) => patients.filter((patient: thisPatient & idPatient) => patient.days.includes(day));
 
   const context = {
     patients,
     patientsByDay,
+    setIsLoggedIn,
   };
 
   return (
