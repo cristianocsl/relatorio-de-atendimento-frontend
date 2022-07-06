09b9cb1717221bb06d93aa934513613019a89ec9
cristianocsl <cslcristiano@gmail.com>
2022-07-06 00:00:10 -0300

    feat: adiciona botão de redirecionar para página de formulário de paciente

--- src/components/PatientsTable.tsx
+++ src/components/PatientsTable.tsx
@@ -6,11 +6,14 @@ import {
   TabList,
   TabPanel,
   TabPanels,
+  Flex,
+  Box,
 } from '@chakra-ui/react'
 import PatientsList from './PatientsList';
 import MyContext from '../context/MyContext';
 import { useNavigate } from 'react-router-dom';
 import IsLoading from './IsLoading';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const PatientsTable = () => {
@@ -31,46 +34,56 @@ const PatientsTable = () => {
 
   if (isLoading) return <IsLoading />;
   return (
-    <Tabs
-      variant='soft-rounded'
-      colorScheme='red'
-      ml={-7}
-      mr={-7}
-      p={1}>
-      <TabList justifyContent={'center'} ml={6} mr={6}>
-        <Wrap justify={'center'}>
-        <Tab
-          bgGradient="linear(to-r, red.300,purple.100)"
-          width={'5%'}>
-          H
-        </Tab>
+    <Box>
+      <Tabs
+        variant='soft-rounded'
+        colorScheme='red'
+        ml={-7}
+        mr={-7}
+        p={1}>
+        <TabList justifyContent={'center'} ml={6} mr={6}>
+          <Wrap justify={'center'}>
+          <Tab
+            bgGradient="linear(to-r, red.300,purple.100)"
+            width={'5%'}>
+            H
+          </Tab>
+            {
+              weekDays.map(
+                (day: string, index: number) => <Tab
+                  width={'5%'}
+                  key={ index }
+                  bgGradient="linear(to-r, red.300,purple.100)">
+                    { day }
+                  </Tab>
+              )
+            }
+          </Wrap>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <PatientsList filterPatientsByDay={ filterPatientsByDay } day={today}/>
+          </TabPanel>
           {
-            weekDays.map(
-              (day: string, index: number) => <Tab
-                width={'5%'}
-                key={ index }
-                bgGradient="linear(to-r, red.300,purple.100)">
-                  { day }
-                </Tab>
-            )
+            weekDays.map((_day, index: number) => {
+              return (
+                <TabPanel key={index}>
+                  <PatientsList filterPatientsByDay={ filterPatientsByDay } day={index + 1}/>
+                </TabPanel>
+              )
+            })
           }
-        </Wrap>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <PatientsList filterPatientsByDay={ filterPatientsByDay } day={today}/>
-        </TabPanel>
-        {
-          weekDays.map((_day, index: number) => {
-            return (
-              <TabPanel key={index}>
-                <PatientsList filterPatientsByDay={ filterPatientsByDay } day={index + 1}/>
-              </TabPanel>
-            )
-          })
-        }
-      </TabPanels>
-    </Tabs>
+        </TabPanels>
+      </Tabs>
+
+      <Flex
+        position={'fixed'}
+        justifyContent={'center'}
+        width={'100%'}
+      >
+        <AddCircleRoundedIcon sx={{ fontSize: 50}} />
+      </Flex>
+    </Box>
   );
 }
 
