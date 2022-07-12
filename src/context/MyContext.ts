433e1008d11d9976b8ba9c87fdbafeff1ddd90a0
cristianocsl <cslcristiano@gmail.com>
2022-07-12 18:05:33 -0300

    feat: adiciona estado de dados de calendario e tipagem no contexto

--- src/context/MyContext.ts
+++ src/context/MyContext.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { createContext } from 'react';
-import { thisPatient, idPatient, thisFinances } from '../services/types';
+import { thisPatient, idPatient, thisFinances, extractDataType } from '../services/types';
 
 type patientArray = thisPatient & idPatient;
 
@@ -10,6 +10,7 @@ const inicialValue = {
   finances: [] as thisFinances[],
   isLoading: true,
   isLoggedIn: false,
+  dataCalendar: [] as extractDataType[],
   setIsLoggedIn: (bool: boolean) => {},
   setIsLoading: (bool: boolean) => {},
   filterPatientsByDay: (day: number) => [] as patientArray[],
