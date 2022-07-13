33cd7ce0881dc9999fb65a5ddeaac0c33d43b2d0
cristianocsl <cslcristiano@gmail.com>
2022-07-13 18:35:17 -0300

    feat: adiciona função que gera mensagem de bom dia, boa tarde, boa noite, a partir do horário

---
+++ src/services/greetingsMessage.ts
@@ -0,0 +1,15 @@
+export default function sendGreetingsMessage(): string {
+  const hoursNow = new Date().getHours();
+
+  const userName = JSON.parse(localStorage.getItem('userName') || '') || 'Usuário(a)';
+
+  if (hoursNow >= 0 && hoursNow < 12) {
+    return `Bom dia, ${userName}!`
+  }
+  if (hoursNow >= 12 && hoursNow < 18) {
+    return `Boa tarde, ${userName}!`
+  }
+  else {
+    return `Boa noite, ${userName}!`
+  }
+}
