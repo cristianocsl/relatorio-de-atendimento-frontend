2e1608daca95099ffd0ae47bf541e6f20a91074c
cristianocsl <cslcristiano@gmail.com>
2022-07-14 19:39:05 -0300

    style: ajustes na largura das colunas de empresa e paciente

--- src/components/PatientsListCards.tsx
+++ src/components/PatientsListCards.tsx
@@ -20,12 +20,13 @@ const PatientsList = (props: Props) => {
         align={'center'}
         fontWeight={'bold'}
         fontSize={'12px'}
+        p={'0 4px 0 4px'}
         justifyContent={'start'}>
         <Box
-          w='60%'
+          w='58%'
           color="wine.7"
           textAlign={'start'}
-          pl={'28px'}
+          pl={'27px'}
         >
           Paciente
         </Box>
@@ -33,7 +34,6 @@ const PatientsList = (props: Props) => {
           w={{ base: '92px', smm: '130px', md: '160px' }}
           wordBreak={'break-word'}
           color="wine.7"
-          mr={'3px'}
         >
           Atend. Sem.
         </Box>
@@ -51,7 +51,7 @@ const PatientsList = (props: Props) => {
           Empr.
         </Box>
         <Box
-          w={{ base: '40%', smm: '50%' }}
+          w={{ base: '40%', smm: '52%' }}
           display={{ base: 'none', ssm: 'block' }}
           color="wine.7"
         >
@@ -60,7 +60,6 @@ const PatientsList = (props: Props) => {
         <Box
           w={{ base: '50px', smm: '100px' }}
           color="wine.7"
-          pr={'5px'}
         >
           Status
         </Box>
@@ -77,8 +76,8 @@ const PatientsList = (props: Props) => {
               p={'4px'}
               bg={'green.1'}
               borderRadius={'4px'}
-              height={{ base: '50px', smm: '50px', md: '60px' }}
-              m={'0 0 10px 0'}>
+              height={{ base: '50px', smm: '40px' }}
+              m={'0 0 16px 0'}>
               <Box
                 w='60%'
                 color="wine.7"
