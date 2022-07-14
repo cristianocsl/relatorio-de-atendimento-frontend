ffa9f9c17a438543b5bbe3e1ec2f70e656b9d77f
cristianocsl <cslcristiano@gmail.com>
2022-07-14 20:38:21 -0300

    feat: adiciona botão de voltar para a tela principal

--- src/components/AddPatient.tsx
+++ src/components/AddPatient.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+import UndoRoundedIcon from '@mui/icons-material/UndoRounded';
+import { useNavigate } from "react-router-dom";
 
 export default function AddPatient () {
+  const navigate = useNavigate();
   return (
-    <div>
+    <Box>
       <h1>Adicionar Paciente</h1>
-    </div>
+      <Button
+        onClick={ () => navigate('/patients') }
+        bg={'wine.8'}
+        _hover={{ bg: 'wine.6' }}
+        color={'red'}
+      >
+        <UndoRoundedIcon sx={{ color: 'wine.main' }} />
+      </Button>
+    </Box>
   );
 }
