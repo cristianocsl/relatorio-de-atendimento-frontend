4a39da77fc4f63ad19fd10e9e4fa80c711825724
cristianocsl <cslcristiano@gmail.com>
2022-07-17 21:59:06 -0300

    feat: adiciona lógica para contar a ocorrência dos dias da semana em cada mês

---
+++ src/services/daysOfMonth.ts
@@ -0,0 +1,34 @@
+import { addDays, subDays, eachDayOfInterval, lastDayOfMonth } from 'date-fns/fp';
+
+const today = new Date();
+
+const extract = (today: Date) => today.toString().split(' ').slice(2, 3)[0]
+const todayInTheMonth: string = extract(today);
+const lastDayInThisMonth:string = extract(lastDayOfMonth(today));
+const quantityDayToTheEnd = +lastDayInThisMonth - +todayInTheMonth;
+
+const firstDay = subDays(+todayInTheMonth - 1, today);
+const lastDay = addDays(+quantityDayToTheEnd, today);
+
+const arrayData = eachDayOfInterval({
+  start: firstDay,
+  end: lastDay,
+});
+
+const extractWeekDay = (data: Date) => data.toString().split(' ').slice(0, 1)[0];
+
+const getDaysQuantity = (day: string, arrayDays: string[]): number => arrayDays.filter((dayInArray) => day === dayInArray).length;
+
+const arrayWeekDays = arrayData.map((data: Date) => extractWeekDay(data));
+
+const objectCounterWeekDays = {
+  1: getDaysQuantity('Sun', arrayWeekDays),
+  2: getDaysQuantity('Mon', arrayWeekDays),
+  3: getDaysQuantity('Tue', arrayWeekDays),
+  4: getDaysQuantity('Wed', arrayWeekDays),
+  5: getDaysQuantity('Thu', arrayWeekDays),
+  6: getDaysQuantity('Fri', arrayWeekDays),
+  7: getDaysQuantity('Sat', arrayWeekDays),
+}
+
+export const dayTest = objectCounterWeekDays;
