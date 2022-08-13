ae2587a425fe12c0fcdc187f421b86097b8e8960
cristianocsl <cslcristiano@gmail.com>
2022-08-12 23:17:28 -0300

    feat: adiciona a lógica de contar pacientes com atendimento pendente para o dia de hoje

--- src/services/counter.ts
+++ src/services/counter.ts
@@ -1,25 +1,26 @@
-import { patientArray } from './types';
+import { patientArray, statusObject } from './types';
 
 function patientsToday(patients: patientArray): number {
-  const today = new Date().getDay();
-  const count = patients.filter((patient) => patient.days.includes(today + 1)).length;
+  const weekDay = new Date().getDay();
+  const count = patients.filter((patient) => patient.days.includes(weekDay + 1)).length;
   return count;
 }
 
 function remainingPatients(patients: patientArray): number {
-  const today = new Date().getDay();
-  const count = patients.filter((patient) => (patient.days.includes(today + 1) && patient.status === '!!!')).length;
-  return count;
-}
-
-function patientsPending(patients: patientArray): number {
-  const count = patients.filter((patient) => (patient.status === '!!!')).length;
-  return count;
+  const weekDay = new Date().getDay();
+  const monthDayToday = new Date().getDate();
+  const patientsToday: patientArray = patients.filter((patient) => patient.days.includes(weekDay + 1));
+  const count: statusObject[] = [];
+  
+  patientsToday.forEach(({ schedule }) => {
+    const info = schedule.filter((daySchedule) => daySchedule.monthDay === monthDayToday && daySchedule.status === '!!!')
+    count.push(...info);
+  });
+  return count.length;
 }
 
 const counter = {
   patientsToday,
-  patientsPending,
   remainingPatients,
 }
 
