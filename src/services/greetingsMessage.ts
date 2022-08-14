c403167673e7b464ec6c6f95c6241a5b6a3883d1
cristianocsl <cslcristiano@gmail.com>
2022-08-14 02:56:14 -0300

    fix: conserta erro de json convertendo o JSON.parse().toString()

--- src/services/greetingsMessage.ts
+++ src/services/greetingsMessage.ts
@@ -1,21 +1,16 @@
 export default function sendGreetingsMessage(): string | undefined {
   const hoursNow = new Date().getHours();
 
-  try {
-    const userNameJson = localStorage.getItem('userName') || '';
-    const userName = JSON.parse(userNameJson).toString();
-  
-    if (hoursNow >= 0 && hoursNow < 12) {
-      return `Bom dia, ${userName}!`
-    }
-    if (hoursNow >= 12 && hoursNow < 18) {
-      return `Boa tarde, ${userName}!`
-    }
-    else {
-      return `Boa noite, ${userName}!`
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  const userNameJson = localStorage.getItem('userName') || '';
+  const userName = JSON.parse(userNameJson).toString();
 
+  if (hoursNow >= 0 && hoursNow < 12) {
+    return `Bom dia, ${userName}!`
+  }
+  if (hoursNow >= 12 && hoursNow < 18) {
+    return `Boa tarde, ${userName}!`
+  }
+  else {
+    return `Boa noite, ${userName}!`
+  }
 }
