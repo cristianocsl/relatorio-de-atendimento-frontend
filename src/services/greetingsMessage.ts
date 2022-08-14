e6b1713f7c81a5d7d309c606a11a802fa4d68798
cristianocsl <cslcristiano@gmail.com>
2022-08-14 02:09:20 -0300

    fix: adiciona JSON.parse

--- src/services/greetingsMessage.ts
+++ src/services/greetingsMessage.ts
@@ -1,7 +1,8 @@
 export default function sendGreetingsMessage(): string {
   const hoursNow = new Date().getHours();
 
-  const userName = localStorage.getItem('userName') || '' || 'Usuário(a)';
+  const userNameJson = localStorage.getItem('userName') || '' || 'Usuário(a)';
+  const userName = JSON.parse(userNameJson);
 
   if (hoursNow >= 0 && hoursNow < 12) {
     return `Bom dia, ${userName}!`
