8b7a4b237c7c60a4c74aaf1d0f5dbb7fb3e55b2b
cristianocsl <cslcristiano@gmail.com>
2022-09-04 12:30:24 -0300

    fix: remove strict mode do react para teste se o nome do usuário é renderizado sem problemas

--- src/index.tsx
+++ src/index.tsx
@@ -12,15 +12,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={themeMaterial}>
+  <ThemeProvider theme={themeMaterial}>
     <ChakraProvider theme={theme}>
       <Provider>
         <App />
       </Provider>
     </ChakraProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
